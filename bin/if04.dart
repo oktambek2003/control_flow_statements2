34f404a99b5e7098cfaf7b3178f6d621bd2e910e
oktambek2003 <anvaralimnazarov84@gmail.com>
2022-10-29 02:24:00 +0500

    4-misol

--- bin/if04.dart
+++ bin/if04.dart
@@ -8,5 +8,28 @@ Returns:
     int: return answer. 
 */
 
+int func(int a, int b, int c) {
+  int d=0 ;
+  if (a == b && b == c && a == c) {
+    if (a > b) {
+      if (a > c) {
+        d = a;
+      } else {
+        d = c;
+      }
+    } else {
+      if (b > c) {
+        d = b;
+      } else {
+        d = c;
+      }
+    }
+    d = 0;
+  }
+
+  return d;
+}
+
 void main() {
+  print(func(92, 92, 92));
 }
