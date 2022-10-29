16ea2d4ef7cfd7001c1ce10f4d2058a7cd818ea5
oktambek2003 <anvaralimnazarov84@gmail.com>
2022-10-29 21:05:19 +0500

    7-misol

--- bin/if07.dart
+++ bin/if07.dart
@@ -14,6 +14,39 @@ Args:
 Returns:
     str: return answer. 
 */
+String func(int a) {
+  
+  String c = 'Monday';
+  String b = 'Tuesday';
+  String d = 'Wednesday';
+  String e = 'Thursday';
+  String f = 'Friday';
+  String i = 'Saturday';
+  String j = 'Sunday';
+  if (a == 1) {
+    return c;
+  }
+  if (a == 2) {
+    return b;
+  }
+  if (a == 3) {
+    return d;
+  }
+  if (a == 4) {
+    return e;
+  }
+  if (a == 5) {
+    return f;
+  }
+  if (a == 6) {
+    return i;
+  }
+  if (a == 7) {
+    return j;
+  }
+  return 'salom';
+}
 
 void main() {
+  print(func(4));
 }
