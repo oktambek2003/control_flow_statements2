bcebd16a10386948ce8af776ceadf77f465e4088
oktambek2003 <anvaralimnazarov84@gmail.com>
2022-10-29 21:06:21 +0500

    1-misol

--- bin/if01.dart
+++ bin/if01.dart
@@ -8,5 +8,27 @@ Args:
 Returns:
     int: return answer.
 */
+int func(int a, int b, int c) {
+  int d;
+  if (a > b) {
+    if (a > c) {
+      d = a;
+    } else {
+      d = c;
+    }
+  }
+    else {
+      if(b > c) {
+    d = b;
+  } else {
+    d = c;
+  }
+    }
+  
+  
+  return d;
+}
+
 void main() {
+  print(func(92, 93, 43));
 }
