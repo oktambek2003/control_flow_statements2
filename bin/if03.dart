25d1ac88ab54a37de4f23fb5b1df16f4d67263d9
oktambek2003 <anvaralimnazarov84@gmail.com>
2022-10-29 21:07:18 +0500

    3-misol

--- bin/if03.dart
+++ bin/if03.dart
@@ -7,5 +7,18 @@ Args:
     c: Third number.
 Returns:
     int: return answer. */
+int func(int a, int b, int c) {
+  int r;
+  if (a > b && a < c || a > c && a < b) {
+    r = a;
+  } else if (b > a && b < c || b<a && b>c) {
+    r = b;
+  } else {
+    r = c;
+  }
+  return r;
+}
+
 void main() {
+  print(func(53, 3, 43));
 }
